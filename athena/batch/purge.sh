3e41192a7379e42929ac3398878f74fc867efaf2
Nicolas RENARD <nrenard@tp01.renard.intra>
2022-06-20 09:24:22 +0200

    V2

--- athena/batch/purge.sh
+++ athena/batch/purge.sh
@@ -7,7 +7,9 @@ else
 fi
 
 logFile="jnlPurge.log"
-jnlPurge="${appPath}/log/${logFile}"
+archDir=$(date +"%Y%m%d-%H%M%S")
+jnlPurge="${appPath}/log/${archDir}/${logFile}"
+typeset -i nbArchivedFiles=0
 typeset -i nbPurgeFiles=0
 
 function fun_writeLog {
@@ -19,6 +21,24 @@ function fun_writeLog {
 if [ ! -d "${appPath}/log/${archDir}" ] ; then
     mkdir "${appPath}/log/${archDir}"
 fi
+
+fun_writeLog "*******************************************"
+fun_writeLog " Démarrage de l'archivage : $(date +"%Y/%m/%d %T")"
+fun_writeLog "*******************************************"
+
+fun_writeLog "Archivage de ${appPath}/log..."
+for file in $(find ${appPath}/log -maxdepth 1 -type f -name "*.log" ) ; do
+    fun_writeLog "    Deplacement de ${file} dans ${appPath}/log/${archDir}/"
+    mv ${file} "${appPath}/log/${archDir}/"
+    if [[ $? -eq 0 ]] ; then
+        nbArchivedFiles=$((nbArchivedFiles + 1))
+    fi
+done
+
+fun_writeLog "Nombre de fichier archivé : ${nbArchivedFiles}"
+fun_writeLog "*******************************************"
+fun_writeLog " Fin de l'archivage : $(date +"%Y/%m/%d %T")"
+fun_writeLog "*******************************************"
 
 fun_writeLog "*******************************************"
 fun_writeLog " Démarrage de la purge : $(date +"%Y/%m/%d %T")"
